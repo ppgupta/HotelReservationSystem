59ae1fbe8686e3755cb340e884dc3884c87fe2bb
ppgupta <premprakashnayak69@gmail.com>
2020-10-16 20:57:10 +0530

    first commit

---
+++ src/main/java/com/capgemini/hotelreservationsystemworkshop/Hotel.java
@@ -0,0 +1,5 @@
+package com.capgemini.hotelreservationsystemworkshop;
+
+public class Hotel {
+
+}
---
+++ src/main/java/com/capgemini/hotelreservationsystemworkshop/HotelReservationMain.java
@@ -0,0 +1,13 @@
+package com.capgemini.hotelreservationsystemworkshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservationMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
